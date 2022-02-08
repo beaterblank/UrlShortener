ef277951554553431a1acbe1d726ade51e23e7b2
G.Mohan Teja <mohanteja.g2019@vitstudent.ac.in>
2022-02-08 15:34:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express');
-
 const app = express();
 app.listen(3000, () => console.log('working'))
 app.set('view engine', 'ejs');
 app.use(express.static("Public"))
-
-
-app.get("/1", (req, res) => {
-    res.redirect('https://www.google.com');
-})
 app.get("/r", (req, res) => {
     let data = req.query.url
     const buff = Buffer.from(data, 'base64');
